fa80985bffa275c88e569d648f2ad5a5ad1d58ae
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2023-03-15 12:20:43 -0500

    Fix slowness when adding source

--- src/private/Write-Package.ps1
+++ src/private/Write-Package.ps1
@@ -9,11 +9,15 @@ function Write-Package {
 		$Request = $Request
 	)
 
+	begin {
+		$sources = Foil\Get-ChocoSource
+	}
+
 	process {
 		foreach ($package in $InputObject) {
 			if ($package.Source) {
 				# If source information is provided (usually from Find-ChocoPackage), construct a source object for inclusion in the results
-				$source = $Request.NewSourceInfo($package.Source,(Foil\Get-ChocoSource | Where-Object Name -EQ $package.Source | Select-Object -ExpandProperty Location),$true)
+				$source = $Request.NewSourceInfo($package.Source,($sources | Where-Object Name -EQ $package.Source | Select-Object -ExpandProperty Location),$true)
 				$Request.WritePackage($package.Name, $package.Version, '', $source)
 			} else {
 				$Request.WritePackage($package.Name, $package.Version)
